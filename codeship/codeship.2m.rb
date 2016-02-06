fd3148184dcdb22ac1b67b92a3283e2f5f4cdce5
Greg Fox <gfox@gf-tech.com>
2016-02-05 21:33:29 -0800

    Added the header information

--- codeship/codeship.2m.rb
+++ codeship/codeship.2m.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
-#
+
+# <bitbar.title>Codeship Build Monitor</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Greg Fox</bitbar.author>
+# <bitbar.author.github>greg-fox</bitbar.author.github>
+# <bitbar.desc>Watches builds on the Codeship server.</bitbar.desc>
+# <bitbar.dependencies>ruby</bitbar.dependencies>
+
 require 'net/http'
 require 'uri'
 require 'open-uri'
@@ -11,6 +18,9 @@ CODESHIP_ACCESS_TOKEN=''
 # ID for your Codeship project
 CODESHIP_PROJECT_ID=''
 
+# Your github account name
+GITHUB_ACCOUNT = ''
+
 CODESHIP_BASE_URL = 'https://codeship.com'
 CODESHIP_PROJECTS_API = "#{CODESHIP_BASE_URL}/api/v1/projects/#{CODESHIP_PROJECT_ID}.json?api_key=#{CODESHIP_ACCESS_TOKEN}"
 CODESHIP_BUILDS_API = "#{CODESHIP_BASE_URL}/api/v1/builds.json?api_key=#{CODESHIP_ACCESS_TOKEN}&project_id=#{CODESHIP_PROJECT_ID}"
@@ -18,8 +28,9 @@ CODESHIP_BUILDS_API = "#{CODESHIP_BASE_URL}/api/v1/builds.json?api_key=#{CODESHI
 CODESHIP_BUILD_URL = "#{CODESHIP_BASE_URL}/projects/#{CODESHIP_PROJECT_ID}/builds/"
 
 LINE_SEPERATOR = 'ꞁ'
-INDICATOR = '★'
 ANCHOR = '⚓'
+YOUR_BUILD_INDICATOR = '☆'
+
 SUCCESS = 'success'
 WAITING = 'waiting'
 TESTING = 'testing'
