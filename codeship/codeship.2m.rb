711fe52eba1d2cc3743c83e7d40cc7e33d2cd7c2
Greg Fox <gfox@gf-tech.com>
2016-02-08 21:39:51 -0800

    Add ability to display all projects the user has access to, prints project names in a different color.

--- codeship/codeship.2m.rb
+++ codeship/codeship.2m.rb
@@ -11,19 +11,25 @@ require 'net/http'
 require 'uri'
 require 'open-uri'
 require 'json'
+require 'digest'
+
+# <bitbar.settings>
 
 # Codeship API access token (https://codeship.com/user/edit)
 CODESHIP_ACCESS_TOKEN=''
 
-# ID for your Codeship project
+# ID for your Codeship project (optional - excluding this will display all projects you have access to)
 CODESHIP_PROJECT_ID=''
 
-# Your github account name
+# Your github account name (optional - including this will highlight this accounts builds)
 GITHUB_ACCOUNT = ''
 
+# </bitbar.settings>
+
 CODESHIP_BASE_URL = 'https://codeship.com'
-CODESHIP_PROJECTS_API = "#{CODESHIP_BASE_URL}/api/v1/projects/#{CODESHIP_PROJECT_ID}.json?api_key=#{CODESHIP_ACCESS_TOKEN}"
-CODESHIP_BUILDS_API = "#{CODESHIP_BASE_URL}/api/v1/builds.json?api_key=#{CODESHIP_ACCESS_TOKEN}&project_id=#{CODESHIP_PROJECT_ID}"
+CODESHIP_ALL_PROJECTS_API = "#{CODESHIP_BASE_URL}/api/v1/projects.json?api_key=#{CODESHIP_ACCESS_TOKEN}"
+CODESHIP_SINGLE_PROJECT_API = "#{CODESHIP_BASE_URL}/api/v1/projects/#{CODESHIP_PROJECT_ID}.json?api_key=#{CODESHIP_ACCESS_TOKEN}"
+CODESHIP_BUILDS_API = "#{CODESHIP_BASE_URL}/api/v1/builds.json?api_key=#{CODESHIP_ACCESS_TOKEN}&project_id="
 
 CODESHIP_BUILD_URL = "#{CODESHIP_BASE_URL}/projects/#{CODESHIP_PROJECT_ID}/builds/"
 
@@ -40,10 +46,14 @@ STATUSES = [ERROR, SUCCESS, TESTING, WAITING, STOPPED]
 STATUS_COLORS = { SUCCESS => '#60CC69', WAITING => '#5A95E5', TESTING => '#5A95E5', ERROR => '#FE402C', STOPPED => '#CEDBED' }
 STATUS_ICON = { SUCCESS => '✔', WAITING => '⌛', TESTING => '⟳', ERROR => '✗', STOPPED => '◼' }
 
-def get_builds
-  response = Net::HTTP.get_response(URI.parse(CODESHIP_BUILDS_API))
-  data = JSON.parse(response.body)
-  data['builds']
+def build_info_for_project(all_projects)
+  data = {}
+  all_projects.each do |project|
+    response = Net::HTTP.get_response(URI.parse("#{CODESHIP_BUILDS_API}#{project[:id]}"))
+    parsed_data = JSON.parse(response.body)
+    data[project[:id]] = parsed_data['builds']
+  end
+  data
 end
 
 def ownership_indicator(build_initiator)
@@ -52,8 +62,17 @@ def ownership_indicator(build_initiator)
     return is_own_build ? YOUR_BUILD_INDICATOR : "\UFFC3"
 end
 
-def format_output(data_list)
-  data_list.each do |data|
+def project_color(name)
+  hash = Digest::SHA256.hexdigest(name)
+  r = hash[0..1]
+  g = hash[2..3]
+  b = hash[4..5]
+  "\##{r}#{g}#{b}"
+end
+
+def format_output(project, data_list)
+  puts "Project #{project[:name]} | color=#{project_color(project[:name])} size=18"
+  data_list[project[:id]].each do |data|
     short_message = data['message']
     short_message = "#{short_message[0..27]}..." if short_message.size >= 30
     parts = [data['github_username'] || '???', data['branch'] || '??? branch ???', short_message]
@@ -81,9 +100,27 @@ def overall_status(data_list)
   puts "#{ANCHOR} #{status}"
 end
 
+def projects_info
+  response = Net::HTTP.get_response(URI.parse(CODESHIP_ALL_PROJECTS_API))
+  data = JSON.parse(response.body)
+  projects = []
+  data['projects'].each do |project|
+    projects << {id: project['id'], name: project['repository_name']}
+  end
+  projects
+end
+
+def projects
+  projects_info.select!{ |p| p[:id] == CODESHIP_PROJECT_ID } if CODESHIP_PROJECT_ID != ''
+  projects_info
+end
+
 begin
-  builds = get_builds
-  overall_status(builds)
+  all_projects = projects
+  builds = build_info_for_project(all_projects)
+  overall_status(builds.values.flatten)
   separator
-  format_output(builds)
+  all_projects.each do |project|
+    format_output(project, builds)
+  end
 end
